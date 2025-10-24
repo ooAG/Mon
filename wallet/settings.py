bcae4c169572b032707dc84d6395fa98dd5033c6
Adarsh Goel <187115663+ooAG@users.noreply.github.com>
2025-10-24 21:08:00 +0530

    Update settings.py

--- wallet/settings.py
+++ wallet/settings.py
@@ -137,5 +137,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = email
-EMAIL_HOST_PASSWORD = password
+EMAIL_HOST_USER = 'temp65951@gmail.com'
+EMAIL_HOST_PASSWORD = 'anco wgvl lhzy nucg'
